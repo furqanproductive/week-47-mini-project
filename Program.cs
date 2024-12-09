59805020aeeccecb2c6a580c17596f94dc636f63
AzureAD\FurqanRiaz <furqan.riaz@enkla.com>
2024-12-09 06:10:36 +0100

    Completed the first level check

--- Program.cs
+++ Program.cs
@@ -1,1 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+
+string[] productsList = new string[0];
+int productsListSize = 0;
+
+while (true)
+{
+    Console.WriteLine("Skriv in produkter. Avsluta med att skriva \'exit\'");
+    string product = Console.ReadLine();
+    //stop getting product from the user when they entered exit
+    if (product == "exit")
+    {
+        break;
+    }
+
+    productsListSize++;
+    Array.Resize(ref productsList, productsListSize);
+    int index = productsListSize - 1;
+    productsList[index] = product;
+}
+
+Console.WriteLine("Du angav följande produkter:");
+
+// Disply the entered products list
+foreach (string product in productsList)
+{ 
+    Console.WriteLine("* " + product); 
+}
+
+Console.WriteLine("Press enter to continue...");
+
+Console.ReadLine();
